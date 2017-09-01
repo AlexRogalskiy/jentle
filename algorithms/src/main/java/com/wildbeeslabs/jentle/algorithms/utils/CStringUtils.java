5a4eaecc3762161499e9554bd3dfc6acc04cbc05
Alexander Rogalsky <alexander.rogalsky@yandex.ru>
2017-09-02 01:28:21 +0300

    Changeset to String Utils

--- algorithms/src/main/java/com/wildbeeslabs/jentle/algorithms/utils/CStringUtils.java
+++ algorithms/src/main/java/com/wildbeeslabs/jentle/algorithms/utils/CStringUtils.java
@@ -3,6 +3,7 @@ package com.wildbeeslabs.jentle.algorithms.utils;
 import java.util.HashSet;
 import java.util.Map;
 import java.util.Objects;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 
@@ -40,5 +41,64 @@ public class CStringUtils {
         Map<Character, Long> firstMap = first.codePoints().mapToObj(ch -> (char) ch).collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
         Map<Character, Long> secondMap = last.codePoints().mapToObj(ch -> (char) ch).collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
         return firstMap.equals(secondMap);
+    }
+
+    public static boolean isPermutationOfPalindrome(final String value) {
+        if (Objects.isNull(value)) {
+            return false;
+        }
+        final AtomicInteger count = new AtomicInteger();
+        Map<Character, Long> valueMap = value.codePoints().mapToObj(ch -> (char) ch).collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+        valueMap.forEach((k, v) -> {
+            if (v % 2 == 1) {
+                if (count.get() > 1) {
+                    return;
+                }
+                count.incrementAndGet();
+            }
+        });
+        return count.get() <= 1;
+    }
+
+    public static boolean isSimilarByOneEdit(final String first, final String last) {
+        if (Objects.isNull(first) || Objects.isNull(last) || Math.abs(first.length() - last.length()) > 1) {
+            return false;
+        }
+        int index1 = 0, index2 = 0;
+        String s1 = first.length() < last.length() ? first : last;
+        String s2 = first.length() < last.length() ? last : first;
+        boolean flag = false;
+        while (index2 < s2.length() && index1 < s1.length()) {
+            if (s1.codePointAt(index1) != s2.codePointAt(index2)) {
+                if (flag) {
+                    return false;
+                }
+                flag = true;
+                if (s1.length() == s2.length()) {
+                    index1++;
+                }
+            } else {
+                index1++;
+            }
+            index2++;
+        }
+        return true;
+    }
+
+    public static String compress(final String value) {
+        if (Objects.isNull(value)) {
+            return null;
+        }
+        StringBuilder compStr = new StringBuilder();
+        int count = 0, len = value.length();
+        for (int i = 0; i < len; i++) {
+            count++;
+            if (i + 1 >= value.length() || value.charAt(i) != value.charAt(i + 1)) {
+                compStr.append(value.charAt(i));
+                compStr.append(count);
+                count = 0;
+            }
+        }
+        return compStr.length() < len ? compStr.toString() : value;
     }
 }
