75e103509efb5e75ae36868e0a538730307ddcb1
Alexander Rogalsky <alexander.rogalsky@yandex.ru>
2017-12-16 04:53:11 +0300

    Changeset to CMisc

--- algorithms/src/main/java/com/wildbeeslabs/jentle/algorithms/misc/CMisc.java
+++ algorithms/src/main/java/com/wildbeeslabs/jentle/algorithms/misc/CMisc.java
@@ -326,4 +326,72 @@ public final class CMisc {
         }
         return lengths;
     }
+
+    @Data
+    @EqualsAndHashCode(callSuper = false)
+    @NoArgsConstructor
+    @ToString
+    public static class Square<T extends Point> {
+
+        private double left;
+        private double right;
+        private double top;
+        private double bottom;
+        private double size;
+
+        public Square(double left, double top, double right, double bottom) {
+            this.left = left;
+            this.top = top;
+            this.right = right;
+            this.bottom = bottom;
+        }
+
+        public Point middle() {
+            return new Point((this.left + this.right) / 2.0, (this.top + this.bottom) / 2.0);
+        }
+
+        public Point extend(final Point mid1, Point mid2, double size) {
+            double xdir = mid1.x < mid2.x ? -1 : 1;
+            double ydir = mid1.y < mid2.x ? -1 : 1;
+            if (mid1.x == mid2.x) {
+                return new Point(mid1.x, mid1.y + ydir * size / 2.0);
+            }
+
+            double slope = (mid1.y - mid2.y) / (mid1.x - mid2.x);
+            @SuppressWarnings("UnusedAssignment")
+            double x1 = 0;
+            @SuppressWarnings("UnusedAssignment")
+            double y1 = 0;
+            if (Math.abs(slope) == 1) {
+                x1 = mid1.x + xdir * size / 2.0;
+                y1 = mid1.y + ydir * size / 2.0;
+            } else if (Math.abs(slope) < 1) {
+                x1 = mid1.x + xdir * size / 2.0;
+                y1 = slope * (x1 - mid1.x) + mid1.y;
+            } else {
+                y1 = mid1.y + ydir * size / 2.0;
+                x1 = (y1 - mid1.y) / slope + mid1.x;
+            }
+            return new Point(x1, y1);
+        }
+
+        public Line cut(final Square<T> other) {
+            final Point p1 = extend(this.middle(), other.middle(), this.size);
+            final Point p2 = extend(this.middle(), other.middle(), -1 * this.size);
+            final Point p3 = extend(other.middle(), this.middle(), other.size);
+            final Point p4 = extend(other.middle(), this.middle(), -1 * other.size);
+
+            Point start = p1;
+            Point end = p1;
+            Point[] points = {p2, p3, p4};
+            for (Point point : points) {
+                if (point.x < start.x || (point.x == start.x && point.y < start.y)) {
+                    start = point;
+                } else if (point.x > end.x || (point.x == end.x && point.y > end.y)) {
+                    end = point;
+                }
+            }
+            return new Line<>(start, end);
+        }
+    }
 }
