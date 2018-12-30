1572ba7b81dc07d3d83bb07bf151ee60099a41c2
Alexander Rogalsky <alexander.rogalsky@yandex.ru>
2018-12-30 14:28:08 +0300

    Changeset to bucket

--- algorithms/src/main/java/com/wildbeeslabs/jentle/algorithms/converter/CByteArrayInOutStream.java
+++ algorithms/src/main/java/com/wildbeeslabs/jentle/algorithms/converter/CByteArrayInOutStream.java
@@ -30,16 +30,16 @@ import java.io.ByteArrayOutputStream;
  * This class extends the ByteArrayOutputStream by providing a method that
  * returns a new ByteArrayInputStream which uses the internal byte array buffer.
  * This buffer is not copied, so no additional memory is used. After creating
- * the ByteArrayInputStream the instance of the ByteArrayInOutStream can not be
+ * the ByteArrayInputStream the instance of the CByteArrayInOutStream can not be
  * used anymore.
  * <p>
  * The ByteArrayInputStream can be retrieved using
  * <code>getInputStream()</code>.
  */
-public final class ByteArrayInOutStream extends ByteArrayOutputStream {
+public final class CByteArrayInOutStream extends ByteArrayOutputStream {
 
     /*
-     ByteArrayInOutStream baios = new ByteArrayInOutStream();
+     CByteArrayInOutStream baios = new CByteArrayInOutStream();
      ByteArrayInputStream inputStream;
 		
      try {
@@ -62,30 +62,30 @@ public final class ByteArrayInOutStream extends ByteArrayOutputStream {
      }
      */
     /**
-     * Creates a new ByteArrayInOutStream. The buffer capacity is initially 32
+     * Creates a new CByteArrayInOutStream. The buffer capacity is initially 32
      * bytes, though its size increases if necessary.
      */
-    public ByteArrayInOutStream() {
+    public CByteArrayInOutStream() {
         super();
     }
 
     /**
-     * Creates a new ByteArrayInOutStream, with a buffer capacity of the
+     * Creates a new CByteArrayInOutStream, with a buffer capacity of the
      * specified size, in bytes.
      *
      * @param size the initial size.
      * @exception IllegalArgumentException if size is negative.
      */
-    public ByteArrayInOutStream(int size) {
+    public CByteArrayInOutStream(int size) {
         super(size);
     }
 
     /**
      * Creates a new ByteArrayInputStream that uses the internal byte array
-     * buffer of this ByteArrayInOutStream instance as its buffer array. The
+     * buffer of this CByteArrayInOutStream instance as its buffer array. The
      * initial value of pos is set to zero and the initial value of count is the
      * number of bytes that can be read from the byte array. The buffer array is
-     * not copied. This instance of ByteArrayInOutStream can not be used anymore
+     * not copied. This instance of CByteArrayInOutStream can not be used anymore
      * after calling this method.
      *
      * @return the ByteArrayInputStream instance
