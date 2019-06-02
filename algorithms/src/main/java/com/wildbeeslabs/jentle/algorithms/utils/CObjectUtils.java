46e97a97cacb46ae4f290b27ed50599a280fed56
Alexander Rogalsky <alexander.rogalsky@yandex.ru>
2019-06-02 18:03:17 +0300

    Changeset to project structure

--- algorithms/src/main/java/com/wildbeeslabs/jentle/algorithms/utils/CObjectUtils.java
+++ algorithms/src/main/java/com/wildbeeslabs/jentle/algorithms/utils/CObjectUtils.java
@@ -27,6 +27,7 @@ import com.google.common.collect.Lists;
 import lombok.experimental.UtilityClass;
 import lombok.extern.slf4j.Slf4j;
 
+import java.lang.reflect.Array;
 import java.util.List;
 import java.util.Objects;
 import java.util.function.Supplier;
@@ -81,5 +82,37 @@ public class CObjectUtils {
     @SuppressWarnings("unchecked")
     public static <T> T cast(final Object object) {
         return (T) object;
+    }
+
+    public static boolean areEqual(final Object o1, final Object o2) {
+        if (o1 == o2) {
+            return true;
+        } else if (o1 == null || o2 == null) {
+            return o1 == null && o2 == null;
+        } else if (isArray(o1)) {
+            return isArray(o2) && areArraysEqual(o1, o2);
+        } else {
+            return o1.equals(o2);
+        }
+    }
+
+    public static boolean areArraysEqual(final Object o1, final Object o2) {
+        return areArrayLengthsEqual(o1, o2)
+            && areArrayElementsEqual(o1, o2);
+    }
+
+    public static boolean areArrayLengthsEqual(final Object o1, final Object o2) {
+        return Array.getLength(o1) == Array.getLength(o2);
+    }
+
+    public static boolean areArrayElementsEqual(final Object o1, final Object o2) {
+        for (int i = 0; i < Array.getLength(o1); i++) {
+            if (!areEqual(Array.get(o1, i), Array.get(o2, i))) return false;
+        }
+        return true;
+    }
+
+    public static boolean isArray(final Object o) {
+        return o.getClass().isArray();
     }
 }
