f4b86fc2ddc95a1bedaf8788f0b6c023fb4e506f
Alexander Rogalsky <alexander.rogalsky@yandex.ru>
2019-06-03 03:24:37 +0300

    Changeset to project structure

--- algorithms/src/main/java/com/wildbeeslabs/jentle/algorithms/utils/CPropertyUtils.java
+++ algorithms/src/main/java/com/wildbeeslabs/jentle/algorithms/utils/CPropertyUtils.java
@@ -40,6 +40,11 @@ import java.lang.reflect.Method;
 @UtilityClass
 public class CPropertyUtils {
 
+    /**
+     * Empty {@link Object} array
+     */
+    public static final Object[] NO_ARGUMENTS = new Object[0];
+
     public static void setProperty(Object instance, String name, String value) {
         if (instance == null) {
             log.warn("Cannot set property " + name + " with value " + value + ". The target instance is null");
@@ -163,5 +168,37 @@ public class CPropertyUtils {
             throw new IllegalArgumentException("Conversion issue on parameter: " + paramName, e);
         }
         throw new IllegalArgumentException("Unsupported type parameter : " + type);
+    }
+
+    /**
+     * Returns the description of the property with the provided
+     * name on the provided object's interface.
+     *
+     * @return the descriptor of the property, or null if the property does not exist.
+     * @throws IllegalArgumentException if there's a introspection failure
+     */
+    public static PropertyDescriptor getPropertyDescriptor(final String propertyName, final Object fromObj) throws IllegalArgumentException {
+        for (PropertyDescriptor property : propertyDescriptorsFor(fromObj, null)) {
+            if (property.getName().equals(propertyName)) {
+                return property;
+            }
+        }
+        return null;
+    }
+
+    /**
+     * Returns all the property descriptors for the class associated with the given object
+     *
+     * @param fromObj   Use the class of this object
+     * @param stopClass Don't include any properties from this ancestor class upwards.
+     * @return Property descriptors
+     * @throws IllegalArgumentException if there's a introspection failure
+     */
+    public static PropertyDescriptor[] propertyDescriptorsFor(final Object fromObj, final Class<Object> stopClass) throws IllegalArgumentException {
+        try {
+            return Introspector.getBeanInfo(fromObj.getClass(), stopClass).getPropertyDescriptors();
+        } catch (IntrospectionException e) {
+            throw new IllegalArgumentException("Could not get property descriptors for " + fromObj.getClass(), e);
+        }
     }
 }
