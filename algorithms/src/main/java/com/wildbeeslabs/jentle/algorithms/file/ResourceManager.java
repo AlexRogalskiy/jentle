32a98e1fefddbc37cca7201867c6b32dd67a3c16
Alexander Rogalsky <alexander.rogalsky@yandex.ru>
2019-06-16 14:59:08 +0300

    Changeset to project structure

---
+++ algorithms/src/main/java/com/wildbeeslabs/jentle/algorithms/file/ResourceManager.java
@@ -0,0 +1,11 @@
+package com.wildbeeslabs.jentle.algorithms.file;
+
+/**
+ * Resource manager interface declaration
+ */
+public interface ResourceManager {
+
+    void before() throws Throwable;
+
+    void after();
+}
