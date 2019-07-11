e9f655d030b847d1106b1e055b6a2a1fee2d578a
Alexander Rogalsky <alexander.rogalsky@yandex.ru>
2019-07-11 12:15:06 +0300

    Changeset to project structure

--- algorithms/src/main/java/com/wildbeeslabs/jentle/algorithms/html/Wikipedia.java
+++ algorithms/src/main/java/com/wildbeeslabs/jentle/algorithms/html/Wikipedia.java
@@ -1,28 +1,28 @@
-package com.wildbeeslabs.jentle.algorithms.html;
-
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
-import org.jsoup.select.Elements;
-
-import java.io.IOException;
-
-/**
- * A simple example, used on the jsoup website.
- */
-public class Wikipedia {
-
-    public static void main(final String[] args) throws IOException {
-        final Document doc = Jsoup.connect("http://en.wikipedia.org/").get();
-        log(doc.title());
-        final Elements newsHeadlines = doc.select("#mp-itn b a");
-        for (final Element headline : newsHeadlines) {
-            log("%s\n\t%s", headline.attr("title"), headline.absUrl("href"));
-        }
-    }
-
-    @SuppressWarnings("non-vargs")
-    private static void log(final String msg, final String... vals) {
-        System.out.println(String.format(msg, vals));
-    }
-}
+//package com.wildbeeslabs.jentle.algorithms.html;
+//
+//import org.jsoup.Jsoup;
+//import org.jsoup.nodes.Document;
+//import org.jsoup.nodes.Element;
+//import org.jsoup.select.Elements;
+//
+//import java.io.IOException;
+//
+///**
+// * A simple example, used on the jsoup website.
+// */
+//public class Wikipedia {
+//
+//    public static void main(final String[] args) throws IOException {
+//        final Document doc = Jsoup.connect("http://en.wikipedia.org/").get();
+//        log(doc.title());
+//        final Elements newsHeadlines = doc.select("#mp-itn b a");
+//        for (final Element headline : newsHeadlines) {
+//            log("%s\n\t%s", headline.attr("title"), headline.absUrl("href"));
+//        }
+//    }
+//
+//    @SuppressWarnings("non-vargs")
+//    private static void log(final String msg, final String... vals) {
+//        System.out.println(String.format(msg, vals));
+//    }
+//}
