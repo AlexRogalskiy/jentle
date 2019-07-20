23bbdb9d10a866767947062295661ba66c408f97
Alexander Rogalsky <alexander.rogalsky@yandex.ru>
2019-07-20 16:30:56 +0300

    Changeset to project structure

--- algorithms/src/main/java/com/wildbeeslabs/jentle/algorithms/utils/RegexUtils.java
+++ algorithms/src/main/java/com/wildbeeslabs/jentle/algorithms/utils/RegexUtils.java
@@ -78,13 +78,11 @@ public class RegexUtils {
     };
 
     /**
-     * Default boolean regular expression
+     * Default boolean regex pattern
      */
     public static final Pattern DEFAULT_BOOL_REGEX = Pattern.compile("^(?:yes|Yes|YES|no|No|NO|true|True|TRUE|false|False|FALSE|on|On|ON|off|Off|OFF)$");
-
     /**
-     * The regular expression is taken from the 1.2 specification but '_'s are
-     * added to keep backwards compatibility
+     * Default regex patterns
      */
     public static final Pattern DEFAULT_FLOAT_REGEX = Pattern.compile("^([-+]?(\\.[0-9]+|[0-9_]+(\\.[0-9_]*)?)([eE][-+]?[0-9]+)?|[-+]?[0-9][0-9_]*(?::[0-5]?[0-9])+\\.[0-9_]*|[-+]?\\.(?:inf|Inf|INF)|\\.(?:nan|NaN|NAN))$");
     public static final Pattern DEFAULT_INT_REGEX = Pattern.compile("^(?:[-+]?0b[0-1_]+|[-+]?0[0-7_]+|[-+]?(?:0|[1-9][0-9_]*)|[-+]?0x[0-9a-fA-F_]+|[-+]?[1-9][0-9_]*(?::[0-5]?[0-9])+)$");
@@ -94,4 +92,9 @@ public class RegexUtils {
     public static final Pattern DEFAULT_TIMESTAMP_REGEX = Pattern.compile("^(?:[0-9][0-9][0-9][0-9]-[0-9][0-9]-[0-9][0-9]|[0-9][0-9][0-9][0-9]-[0-9][0-9]?-[0-9][0-9]?(?:[Tt]|[ \t]+)[0-9][0-9]?:[0-9][0-9]:[0-9][0-9](?:\\.[0-9]*)?(?:[ \t]*(?:Z|[-+][0-9][0-9]?(?::[0-9][0-9])?))?)$");
     public static final Pattern DEFAULT_VALUE_REGEX = Pattern.compile("^(?:=)$");
     public static final Pattern DEFAULT_YAML_REGEX = Pattern.compile("^(?:!|&|\\*)$");
+    /**
+     * DEfault date/time regex patterns
+     */
+    public final static Pattern TIMESTAMP_REGEXP = Pattern.compile("^([0-9][0-9][0-9][0-9])-([0-9][0-9]?)-([0-9][0-9]?)(?:(?:[Tt]|[ \t]+)([0-9][0-9]?):([0-9][0-9]):([0-9][0-9])(?:\\.([0-9]*))?(?:[ \t]*(?:Z|([-+][0-9][0-9]?)(?::([0-9][0-9])?)?))?)?$");
+    public final static Pattern YMD_REGEXP = Pattern.compile("^([0-9][0-9][0-9][0-9])-([0-9][0-9]?)-([0-9][0-9]?)$");
 }
