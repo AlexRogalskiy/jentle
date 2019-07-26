be93b16aa7838c8f80250fd4a4105f42b2950f90
AlexanderRogalskiy <alexander.rogalskiy@paragon-software.com>
2019-07-26 22:53:03 +0300

    Changeset to project structure

---
+++ algorithms/src/main/java/com/wildbeeslabs/jentle/algorithms/enums/Cover.java
@@ -0,0 +1,10 @@
+package com.wildbeeslabs.jentle.algorithms.enums;
+
+/**
+ * Book cover type {@link Enum} implementation
+ */
+public enum Cover {
+    PAPERBACK,
+    HARDCOVER,
+    DUST_JACKET
+}
