97a7f7ad20dfa2e806afb543d0ec4e43e1b60d31
Alexander Rogalsky <alexander.rogalsky@yandex.ru>
2019-11-02 14:50:50 +0300

    Changeset to project structure

---
+++ algorithms/src/main/java/com/wildbeeslabs/jentle/algorithms/map/CopyOnWriteArrayListMultiMap.java
@@ -0,0 +1,54 @@
+package com.wildbeeslabs.jentle.algorithms.map;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
+
+public class CopyOnWriteArrayListMultiMap<K, V> {
+
+    private final ConcurrentMap<K, List<V>> cache = new ConcurrentHashMap<K, List<V>>();
+
+    public List<V> get(K k) {
+        return cache.get(k);
+    }
+
+    public synchronized List<V> remove(K k) {
+        return cache.remove(k);
+    }
+
+    public synchronized void putIfAbsent(K k, V v) {
+        List<V> list = cache.get(k);
+        if (list == null || list.isEmpty()) {
+            list = new ArrayList<V>();
+        } else {
+            list = new ArrayList<V>(list);
+        }
+        if (!list.contains(v)) {
+            list.add(v);
+            cache.put(k, list);
+        }
+    }
+
+    public synchronized boolean remove(K k, K v) {
+        List<V> list = cache.get(k);
+        if (list == null) {
+            return false;
+        }
+        if (list.isEmpty()) {
+            cache.remove(k);
+            return false;
+        }
+        boolean removed = list.remove(v);
+        if (removed) {
+            if (list.isEmpty()) {
+                cache.remove(k);
+            } else {
+                list = new ArrayList<V>(list);
+                cache.put(k, list);
+            }
+        }
+        return removed;
+    }
+
+}
