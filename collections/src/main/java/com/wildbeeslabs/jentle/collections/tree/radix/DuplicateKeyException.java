560905e8c56b5f42282090cccd47d4f22ca77e6c
Alexander Rogalsky <alexander.rogalsky@yandex.ru>
2019-11-16 21:42:58 +0300

    Changeset to project structure

---
+++ collections/src/main/java/com/wildbeeslabs/jentle/collections/tree/radix/DuplicateKeyException.java
@@ -0,0 +1,17 @@
+package com.wildbeeslabs.jentle.collections.tree.radix;
+
+/**
+ * excepion thrown if a duplicate key is inserted in a {@link RadixTree}
+ *
+ * @author Tahseen Ur Rehman
+ * email: tahseen.ur.rehman {at.spam.me.not} gmail.com
+ */
+public class DuplicateKeyException extends RuntimeException
+{
+        private static final long serialVersionUID = 3141795907493885706L;
+
+        public DuplicateKeyException(String msg)
+        {
+                super(msg);
+        }
+}
